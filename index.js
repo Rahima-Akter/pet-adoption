eb522b7c9dde8c2d02a2f0ce4093bb07d3bf5f42
Rahima-Akter <rahimaakter1527@gmail.com>
2024-10-08 00:54:46 -0700

    adoption & details button functionallity added

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const spinner = () => {
     }, 2000);
 };  // end
 
+// active buttons styles
 const btnStyles = (clickedBtn) => {
     const buttons = document.getElementsByClassName('btn-group');
     for (const btn of buttons) {
@@ -77,7 +78,6 @@ const showPetCard = (pet) => {
     }
 
     pet.forEach(item => {
-
         const div = document.createElement('div');
         cardContainer.classList.add('grid')
         div.innerHTML = `
@@ -97,9 +97,9 @@ const showPetCard = (pet) => {
                         <div class="flex justify-between mt-2">
                             <button onclick="likedImage('${item.image}')" class="px-3 py-2 border border-gray-300 hover:bg-[#0E7A81] rounded-md"><img src="images/thumbs-up.png" alt="">
                             </button>
-                            <button  class="text-sm font-semibold rounded-md px-3 py-2 border border-gray-300 text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white hover:border-[#0E7A81] ">Adopt
+                            <button onclick="adoptionModal(this)" class="text-sm font-semibold rounded-md px-3 py-2 border border-gray-300 text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white hover:border-[#0E7A81] btn-disable">Adopt
                             </button>
-                            <button class="text-sm font-semibold rounded-md px-3 py-2 border border-gray-300 text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white">Details
+                            <button onclick="fetchModalDetails(${item.petId})" class="text-sm font-semibold rounded-md px-3 py-2 border border-gray-300 text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white">Details
                             </button>
                         </div>
                     </div>
@@ -108,9 +108,63 @@ const showPetCard = (pet) => {
         cardContainer.append(div);
     });
 };
+// details modal
+const fetchModalDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+    const data = await res.json()
+    modal(data.petData)
+};
+const modal = (details) => {
+    document.getElementById('myModal').showModal()
+    const modalContainer = document.getElementById('modal-content');
+    modalContainer.innerHTML = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="">
+        <img class="rounded-md w-full h-[250px] object-cover" src="${details.image}"/>
+        </div>
+        <h2 class="card-title mt-4 mx-7 font-bold text-2xl text-black">${details.pet_name}</h2>
+        <div class="text-left text-sm font-semibold mt-3 md:flex justify-between mx-7">
+            <div>
+                <p class="flex gap-2"><span><img src="images/breed.png"/></span>Breed: ${details.breed ? details.breed : 'N/A'}</p>
+                <p class="flex items-center gap-2"><span><img src="images/date.png"/></span>Birth: ${details.date_of_birth ? details.date_of_birth : 'N/A'}</p>
+                <p class="flex gap-2"><span><img src="images/gender.png"/></span>Gender: ${details.gender ? details.gender : 'N/A'}</p>
+            </div>
+            <div>
+                <p class="flex gap-2"><span><img src="images/price.png"/></span>price: ${details.price ? `${details.price}$` : 'N/A'}</p>
+                <p class="flex gap-2"><span><img src="images/gender.png"/></span>vaccinated_status: ${details.vaccinated_status ? `${details.vaccinated_status}` : 'N/A'}</p>
+            </div>
+        </div>
+        <div class="mt-4 mx-7 text-sm">
+            <p class="font-bold text-lg mb-2">Detailed Information:</p>
+            ${details.pet_details}
+        </div>
+    `;
+    modalContainer.appendChild(div)
+}; // end
+
+// count down modal
+const showModal = document.getElementById('adoptionModal');
+const countDown = document.getElementById('countDown');
+const adoptionModal = (btn) => {
+    showModal.showModal();
+    let count = 3;
+    countDown.innerText = count;
+    const timeOut = setInterval(() => {
+        count--;
+        countDown.innerText = count;
+
+        if (count <= 0) {
+            clearInterval(timeOut);
+            showModal.close();
+            // disable the button after countDown
+            btn.setAttribute('disabled', true);
+            btn.classList = 'hover:none btn btn-sm '
+        };
+    }, 1000);
+};
 
 // fetch pets by category
-
 const petCategory = async (name) => {
     try {
         const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${name}`)
@@ -128,6 +182,7 @@ const likedImage = (image) => {
     const div = document.createElement('div')
     div.innerHTML = `<img class="rounded-md" src="${image}" />`;
     imgContainer.appendChild(div);
+    document.getElementById('sideImgContainer-title').style.display = 'none';
 }; // end
 
 btnCategory();
